dbf0757f120a139af031ed7ccdeb7395b9ea311c
= <=>
2021-02-11 10:28:52 +0800

    添加了RecyclerView的依赖

---
+++ app/src/main/java/top/bestguo/androidlayout/recyclerview/RecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package top.bestguo.androidlayout.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.bestguo.androidlayout.R;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
