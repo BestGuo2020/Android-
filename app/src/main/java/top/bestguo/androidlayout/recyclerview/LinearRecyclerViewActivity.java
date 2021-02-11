075d32928840b48054cced694e2a3a668c9d509c
= <=>
2021-02-11 16:35:26 +0800

    解决了一些错误

---
+++ app/src/main/java/top/bestguo/androidlayout/recyclerview/LinearRecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package top.bestguo.androidlayout.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.bestguo.androidlayout.R;
+
+public class LinearRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_recycler_view);
+    }
+}
